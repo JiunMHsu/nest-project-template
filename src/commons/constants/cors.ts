5b07f5b92c60f5cf509785176a995f1b2e84c894
Ming <hsujm219@gmail.com>
2025-04-29 03:13:50 -0300

    Update cors.ts

--- src/commons/constants/cors.ts
+++ src/commons/constants/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 export const cors: CorsOptions = {
     origin: true,
-    methods: 'GET, HEAD, POST, PUT, PATCH, DELETE', // OPTIONS?
+    methods: 'GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS',
     // allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
 };
