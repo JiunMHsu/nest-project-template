fa510efd8e21a21a1d2e6736d4ffb10242506841
Jiun Ming Hsu <hsujm219@gmail.com>
2026-07-26 01:32:28 -0300

    update eslint conf

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,8 +8,8 @@ export default TSEslint.config(
         ignores: ['eslint.config.mjs', 'dist', 'node_modules', 'coverage'],
     },
     eslint.configs.recommended,
-    ...TSEslint.configs.recommendedTypeChecked,
     {
+        extends: [...TSEslint.configs.recommendedTypeChecked],
         languageOptions: {
             globals: {
                 ...globals.node,
@@ -37,6 +37,17 @@ export default TSEslint.config(
                 },
             ],
             // 'prettier/prettier': ['error', { endOfLine: 'auto' }],
+        },
+    },
+    {
+        files: ['**/*.spec.ts'],
+        rules: {
+            '@typescript-eslint/unbound-method': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-call': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
+            '@typescript-eslint/no-unsafe-argument': 'off',
+            '@typescript-eslint/no-unsafe-member-access': 'off',
         },
     },
 );
