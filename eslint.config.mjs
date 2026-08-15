5ebab17256702a49c3d7f1947ca137c5b2a8fa47
Jiun Ming Hsu <hsujm219@gmail.com>
2026-08-15 01:36:47 -0300

    update dependencies and configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,8 +8,8 @@ export default TSEslint.config(
         ignores: ['eslint.config.mjs', 'dist', 'node_modules', 'coverage'],
     },
     eslint.configs.recommended,
+    ...TSEslint.configs.recommendedTypeChecked,
     {
-        extends: [...TSEslint.configs.recommendedTypeChecked],
         languageOptions: {
             globals: {
                 ...globals.node,
@@ -40,14 +40,15 @@ export default TSEslint.config(
         },
     },
     {
-        files: ['**/*.spec.ts'],
+        files: ['test/**/*.ts'],
         rules: {
-            '@typescript-eslint/unbound-method': 'off',
             '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-member-access': 'off',
             '@typescript-eslint/no-unsafe-call': 'off',
             '@typescript-eslint/no-unsafe-return': 'off',
             '@typescript-eslint/no-unsafe-argument': 'off',
-            '@typescript-eslint/no-unsafe-member-access': 'off',
+            '@typescript-eslint/unbound-method': 'off',
+            '@typescript-eslint/require-await': 'off',
         },
     },
 );
