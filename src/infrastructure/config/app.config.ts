b4fa2423210b185ce3708901a43ae0f975837bdd
Jiun Ming Hsu <hsujm219@gmail.com>
2026-09-22 00:14:12 -0300

    drop log level config

--- src/infrastructure/config/app.config.ts
+++ src/infrastructure/config/app.config.ts
@@ -6,7 +6,6 @@ if (existsSync('.env')) process.loadEnvFile();
 
 export const config = {
     env: getOrDefault<'development' | 'production' | 'test'>('NODE_ENV', 'development'),
-    logLevel: getOrDefault<'verbose' | 'debug' | 'log' | 'warn' | 'error' | 'fatal'>('LOG_LEVEL', 'log'),
 
     app: {
         name: getOrDefault('APP_NAME', 'App'),
